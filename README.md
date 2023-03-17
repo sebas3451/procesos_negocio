33d7315999ed320235c04ed799877c9be325d091
sebas3451 <ortis955@gmail.com>
2023-03-17 00:35:57 -0500

    add

---
+++ README.md
@@ -0,0 +1,66 @@
+Red Hat Process Automation Manager 7 Install Demo
+=================================================
+Project to automate the installation of this product without preconfiguration beyond a single admin user.
+
+You can install this project on your own machine or on an OpenShift Container Platform.
+
+
+Installing on local machine
+---------------------------
+1. [Download and unzip](https://github.com/jbossdemocentral/rhpam7-install-demo/archive/master.zip) or [clone this repo](https://github.com/jbossdemocentral/rhpam7-install-demo.git).
+   
+2. Add products to installs directory, see [installs/README](installs/README) for details and links.
+
+3. Run 'init.sh' or 'init.bat' file. 'init.bat' must be run with Administrative privileges.
+
+Follow install output instructions and log in to http://localhost:8080/business-central (u:pamAdmin / p:redhatpam1!)
+
+Enjoy the installed and configured Red Hat Process Automation Manager 7. If you need more help, see the [install video](https://youtu.be/x6U8CiPU9cU).
+
+
+Installing on OpenShift Container Platform (CodeReady Containers)
+----------------------------------------------------------------
+Install this demo on your local machine using an OpenShift Container Platform development cluster can be done using 
+CodeReady Continers. It's fully automated and delivers a fully functioning container-native development experience:
+
+[Code Ready Containers Red Hat Process Automation Easy Install](https://gitlab.com/redhatdemocentral/crc-rhpam-install-demo)
+
+
+Installing on any OpenShift Container Platform (generic)
+-------------------------------------------------------
+This demo can be installed on your own version of OpenShift Container Platform. Options and guidance can be found 
+in [openshift support directory README](support/openshift/README.md).
+
+
+Supporting Articles
+-------------------
+- [Video - Red Hat Process Automation Manager Install Demo](https://youtu.be/x6U8CiPU9cU)
+
+- [CodeReady Containers - Getting Started with OpenShift 4.5 and Process Automation Tooling](https://dzone.com/articles/codeready-containers-getting-started-with-openshif-1)
+
+- [Getting Started with Red Hat Process Automation Manager 7](https://upload.wikimedia.org/wikipedia/commons/6/67/Learning_Curve_--_Coming_Soon_Placeholder.png)
+
+
+Released versions
+-----------------
+See the tagged releases for the following versions of the product:
+
+- v1.12 - Red Hat Process Automation Manager 7.11.0.GA
+- v1.11 - Red Hat Process Automation Manager 7.10.0.GA
+- v1.10 - Red Hat Process Automation Manager 7.9.0.GA
+- v1.9 - Red Hat Process Automation Manager 7.8.0.GA
+- v1.8 - Red Hat Process Automation Manager 7.7.0.GA
+- v1.7 - Red Hat Process Automation Manager 7.5.0.GA
+- v1.6 - Red Hat Process Automation Manager 7.4.0.GA
+- v1.5 - Red Hat Process Automation Manager 7.3.0.GA
+- v1.4 - Red Hat Process Automation Manager 7.3.0.GA
+- v1.3 - Red Hat Process Automation Manager 7.1.0.GA
+- v1.2 - Red Hat Process Automation Manager 7.0.2.GA
+- v0.8 - Red Hat Process Automation Manager 7.0.0.ER2
+
+[![RHPAM VIDEO](docs/demo-images/rhpam7-video.png)](https://youtu.be/x6U8CiPU9cU)
+
+![RHPAM 7](docs/demo-images/rhpam7.png)
+
+![RHPAM 7 CASE](docs/demo-images/rhpam7-case.png)
+
---
+++ init.sh
@@ -0,0 +1,241 @@
+#!/bin/sh 
+DEMO="Install Demo"
+AUTHORS="Red Hat"
+PROJECT="git@github.com:jbossdemocentral/rhpam7-install-demo.git"
+PRODUCT="Red Hat Process Automation Manager"
+JBOSS_HOME=./target/jboss-eap-7.3
+SERVER_DIR=$JBOSS_HOME/standalone/deployments
+SERVER_CONF=$JBOSS_HOME/standalone/configuration/
+SERVER_BIN=$JBOSS_HOME/bin
+SRC_DIR=./installs
+SUPPORT_DIR=./support
+PRJ_DIR=./projects
+VERSION_EAP=7.3.0
+VERSION=7.11.0
+EAP=jboss-eap-$VERSION_EAP.zip
+RHPAM=rhpam-$VERSION-business-central-eap7-deployable.zip
+RHPAM_KIE_SERVER=rhpam-$VERSION-kie-server-ee8.zip
+RHPAM_ADDONS=rhpam-$VERSION-add-ons.zip
+RHPAM_CASE=rhpam-$VERSION-case-mgmt-showcase-eap7-deployable.zip
+
+# wipe screen.
+clear 
+
+echo
+echo "###################################################################"
+echo "##                                                               ##"   
+echo "##  Setting up the                                               ##"
+echo "##                                                               ##"   
+echo "##             ####  ##### ####     #   #  ###  #####            ##"
+echo "##             #   # #     #   #    #   # #   #   #              ##"
+echo "##             ####  ###   #   #    ##### #####   #              ##"
+echo "##             #  #  #     #   #    #   # #   #   #              ##"
+echo "##             #   # ##### ####     #   # #   #   #              ##"
+echo "##                                                               ##"
+echo "##           ####  ####   ###   #### #####  ####  ####           ##"
+echo "##           #   # #   # #   # #     #     #     #               ##"
+echo "##           ####  ####  #   # #     ###    ###   ###            ##"
+echo "##           #     #  #  #   # #     #         #     #           ##"
+echo "##           #     #   #  ###   #### ##### ####  ####            ##"
+echo "##                                                               ##"
+echo "##   ###  #   # #####  ###  #   #  ###  ##### #####  ###  #   #  ##"
+echo "##  #   # #   #   #   #   # ## ## #   #   #     #   #   # ##  #  ##"
+echo "##  ##### #   #   #   #   # # # # #####   #     #   #   # # # #  ##"
+echo "##  #   # #   #   #   #   # #   # #   #   #     #   #   # #  ##  ##"
+echo "##  #   # #####   #    ###  #   # #   #   #   #####  ###  #   #  ##"
+echo "##                                                               ##"
+echo "##           #   #  ###  #   #  ###  ##### ##### ####            ##"
+echo "##           ## ## #   # ##  # #   # #     #     #   #           ##"
+echo "##           # # # ##### # # # ##### #  ## ###   ####            ##"
+echo "##           #   # #   # #  ## #   # #   # #     #  #            ##"
+echo "##           #   # #   # #   # #   # ##### ##### #   #           ##"
+echo "##                                                               ##"   
+echo "##  brought to you by, ${AUTHORS}                            ##"
+echo "##                                                               ##"   
+echo "##  ${PROJECT}      ##"
+echo "##                                                               ##"   
+echo "###################################################################"
+echo
+
+# make some checks first before proceeding.	
+if [ -r $SUPPORT_DIR ] || [ -L $SUPPORT_DIR ]; then
+        echo "Support dir is presented..."
+        echo
+else
+        echo "$SUPPORT_DIR wasn't found. Please make sure to run this script inside the demo directory."
+        echo
+        exit
+fi
+
+if [ -r $SRC_DIR/$EAP ] || [ -L $SRC_DIR/$EAP ]; then
+	echo "Product EAP sources are present..."
+	echo
+else
+	echo "Need to download $EAP package from https://developers.redhat.com/products/eap/download"
+	echo "and place it in the $SRC_DIR directory to proceed..."
+	echo
+	exit
+fi
+
+if [ -r $SRC_DIR/$RHPAM ] || [ -L $SRC_DIR/$RHPAM ]; then
+	echo "Product Red Hat Process Automation Manager sources are present..."
+	echo
+else
+	echo "Need to download $RHPAM package from https://developers.redhat.com/products/rhpam/download"
+	echo "and place it in the $SRC_DIR directory to proceed..."
+	echo
+	exit
+fi
+
+if [ -r $SRC_DIR/$RHPAM_KIE_SERVER ] || [ -L $SRC_DIR/$RHPAM_KIE_SERVER ]; then
+	echo "Product Red Hat Process Automation Manager KIE Server sources are present..."
+	echo
+else
+	echo "Need to download $RHPAM_KIE_SERVER package from https://developers.redhat.com/products/rhpam/download"
+	echo "and place it in the $SRC_DIR directory to proceed..."
+	echo
+	exit
+fi
+
+if [ -r $SRC_DIR/$RHPAM_ADDONS ] || [ -L $SRC_DIR/$RHPAM_ADDONS ]; then
+	echo "Product Red Hat Process Automation Manager Case Management sources are present..."
+	echo
+else
+	echo "Need to download $RHPAM_ADDONS package from https://developers.redhat.com/products/rhpam/download"
+	echo "and place it in the $SRC_DIR directory to proceed..."
+	echo
+	exit
+fi
+
+# Remove the old JBoss instance, if it exists.
+if [ -x $JBOSS_HOME ]; then
+		echo "  - removing existing JBoss product..."
+		echo
+		rm -rf $JBOSS_HOME
+fi
+
+# Installation.
+echo "JBoss EAP installation running now..."
+echo
+mkdir -p ./target
+unzip -qo $SRC_DIR/$EAP -d target
+
+if [ $? -ne 0 ]; then
+	echo
+	echo Error occurred during JBoss EAP installation!
+	exit
+fi
+
+echo "Red Hat Process Automation Manager installation running now..."
+echo
+unzip -qo $SRC_DIR/$RHPAM -d target
+
+if [ $? -ne 0 ]; then
+	echo
+	echo Error occurred during Red Hat Process Manager installation!
+	exit
+fi
+
+echo "Red Hat Process Automation Manager Kie Server installation running now..."
+echo
+unzip -qo $SRC_DIR/$RHPAM_KIE_SERVER  -d $JBOSS_HOME/standalone/deployments 
+
+if [ $? -ne 0 ]; then
+	echo
+	echo Error occurred during Red Hat Process Manager Kie Server installation!
+	exit
+fi
+
+# Set deployment Kie Server.
+touch $JBOSS_HOME/standalone/deployments/kie-server.war.dodeploy
+
+echo "Red Hat Process Automation Manager Case Management installation running now..."
+echo
+unzip -qo $SRC_DIR/$RHPAM_ADDONS $RHPAM_CASE -d $SRC_DIR
+unzip -qo $SRC_DIR/$RHPAM_CASE -d target
+rm $SRC_DIR/$RHPAM_CASE
+
+if [ $? -ne 0 ]; then
+	echo
+	echo Error occurred during Red Hat Process Manager Case Management installation!
+	exit
+fi
+
+# Set deployment Case Management.
+touch $JBOSS_HOME/standalone/deployments/rhpam-case-mgmt-showcase.war.dodeploy
+
+echo "  - enabling demo accounts role setup..."
+echo
+echo "  - adding user 'pamAdmin' with password 'redhatpam1!'..."
+echo
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u pamAdmin -p redhatpam1! -ro analyst,admin,manager,user,kie-server,kiemgmt,rest-all --silent
+
+echo "  - adding user 'adminUser' with password 'test1234!'..."
+echo
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u adminUser -p test1234! -ro analyst,admin,manager,user,kie-server,kiemgmt,rest-all --silent
+
+echo "  - adding user 'kieserver' with password 'kieserver1!'..."
+echo
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u kieserver -p kieserver1! -ro kie-server,rest-all --silent
+
+echo "  - adding user 'caseUser' with password 'redhatpam1!'..."
+echo
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u caseUser -p redhatpam1! -ro user --silent
+
+echo "  - adding user 'caseManager' with password 'redhatpam1!'..."
+echo
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u caseManager -p redhatpam1! -ro user,manager --silent
+
+echo "  - adding user 'caseSupplier' with password 'redhatpam1!'..."
+echo
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u caseSupplier -p redhatpam1! -ro user,supplier --silent
+
+echo "  - setting up standalone.xml configuration adjustments..."
+echo
+cp $SUPPORT_DIR/standalone-full.xml $SERVER_CONF/standalone.xml
+
+echo "  - setup email task notification users..."
+echo
+cp $SUPPORT_DIR/userinfo.properties $SERVER_DIR/business-central.war/WEB-INF/classes/
+
+
+echo "  - setup system property for jpa marshaller"
+echo
+$JBOSS_HOME/bin/jboss-cli.sh <<EOT
+embed-server
+/system-property=org.kie.server.xstream.enabled.packages:add(value="org.drools.persistence.jpa.marshaller.*")
+EOT
+
+
+
+# Add execute permissions to the standalone.sh script.
+echo "  - making sure standalone.sh for server is executable..."
+echo
+chmod u+x $JBOSS_HOME/bin/standalone.sh
+
+echo "=============================================================="
+echo "=                                                            ="
+echo "=  $PRODUCT $VERSION setup complete.  ="
+echo "=                                                            ="
+echo "=  Start $PRODUCT with:            ="
+echo "=                                                            ="
+echo "=           $SERVER_BIN/standalone.sh         ="
+echo "=                                                            ="
+echo "=  Log in to Red Hat Process Automation Manager to start     ="
+echo "=  developing rules projects:                                ="
+echo "=                                                            ="
+echo "=  http://localhost:8080/business-central                    ="
+echo "=                                                            ="
+echo "=    Log in: [ u:pamAdmin / p:redhatpam1! ]                  ="
+echo "=                                                            ="
+echo "=  http://localhost:8080/rhpam-case-mgmt-showcase            ="
+echo "=                                                            ="
+echo "=    Others:                                                 ="
+echo "=            [ u:kieserver / p:kieserver1! ]                 ="
+echo "=            [ u:caseUser / p:redhatpam1! ]                  ="
+echo "=            [ u:caseManager / p:redhatpam1! ]               ="
+echo "=            [ u:caseSupplier / p:redhatpam1! ]              ="
+echo "=                                                            ="
+echo "=============================================================="
+echo
+
